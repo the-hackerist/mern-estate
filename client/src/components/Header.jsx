6e7298c3b972561717f0616d8ca572037330f5c6
vince <vincedominic.delapena@gmail.com>
2024-09-03 21:19:21 +0800

    update header bg from slate 200 to 300

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="bg-slate-200 shadow-md">
+    <header className="bg-slate-300 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
