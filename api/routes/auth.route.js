46a195d4775061ab26e2b8058d0a1a06b07e1645
vince <vincedominic.delapena@gmail.com>
2024-09-11 15:22:38 +0800

    add sign out functionality

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { google, signin, signup } from "../controllers/auth.controller.js";
+import {
+  google,
+  signin,
+  signout,
+  signup,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
@@ -8,5 +13,7 @@ router.post("/signup", signup);
 router.post("/signin", signin);
 
 router.post("/google", google);
+
+router.get("/signout", signout);
 
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,9 @@ import {
   deleteUserStart,
   deleteUserSuccess,
   deleteUserFailure,
+  signOutUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 
@@ -114,6 +117,22 @@ function Profile() {
     }
   };
 
+  const handleSignout = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch("api/auth/signout");
+      const data = await res.json();
+
+      if (data.success === false) {
+        dispatch(signOutUserFailure(data.message));
+        return;
+      }
+      dispatch(signOutUserSuccess(data));
+    } catch (error) {
+      dispatch(signOutUserFailure(error.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -183,7 +202,9 @@ function Profile() {
         >
           Delete account
         </span>
-        <span className="text-red-700 cursor-pointer">Sign out</span>
+        <span onClick={handleSignout} className="text-red-700 cursor-pointer">
+          Sign out
+        </span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
       <p className="text-green-700 mt-5">
