b7ebaae10210e17d456c2cb82c93002ae0747e14
vince <vincedominic.delapena@gmail.com>
2024-09-18 18:15:23 +0800

    add onchange ang onsubmit function to the seach page

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -96,7 +96,7 @@ export const getListings = async (req, res, next) => {
     const order = req.query.order || "desc";
 
     const listings = await Listing.find({
-      name: { $regex: searchTerm, $options: i },
+      name: { $regex: searchTerm, $options: "i" },
       offer,
       furnished,
       parking,
