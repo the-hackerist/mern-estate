3042957e817144b48cfd99d96fbb09cdaf3fa029
vince <vincedominic.delapena@gmail.com>
2024-09-18 15:41:48 +0800

    create search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -4,6 +4,7 @@ import {
   deleteListing,
   updateListing,
   getListing,
+  getListings,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -16,5 +17,7 @@ router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
 
 router.get("/get/:id", getListing);
+
+router.get("/get", getListings);
 
 export default router;
