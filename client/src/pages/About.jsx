48deb87493526fa1e33a77476bcf8fc55521ef06
vince <vincedominic.delapena@gmail.com>
2024-09-18 23:39:11 +0800

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,35 @@
 function About() {
-  return <div></div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Sahand Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Sahand Estate is a dedicated real estate agency specializing in
+        residential and commercial property listings. With a team of experienced
+        professionals, we leverage our extensive market knowledge and
+        cutting-edge technology to connect clients with their ideal properties.
+        Whether you&apos;re looking to buy, sell, or rent, we are here to guide
+        you through every step of the journey. Our client-focused approach
+        ensures that your needs and goals are always our top priority. Join us
+        at Sahand Estate, where your real estate dreams become a reality.
+      </p>
+
+      <p className="mb-4 text-slate-700">
+        At Sahand Estate, our mission is to simplify the real estate experience
+        for buyers and sellers alike. We strive to provide comprehensive,
+        transparent, and personalized services that empower our clients to make
+        informed decisions in the ever-evolving property market.
+      </p>
+
+      <p className="mb-4 text-slate-700">
+        Our vision is to be the leading real estate agency recognized for our
+        integrity, innovative solutions, and commitment to excellence. We aim to
+        transform the way people engage with real estate, making the process
+        accessible, efficient, and enjoyable for everyone. Company Overview:
+      </p>
+    </div>
+  );
 }
 
 export default About;
